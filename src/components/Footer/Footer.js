96cc209a0ccf572fcd80f356a0e7f50eeafa8da7
Dare Lawal <lawaldare@gmail.com>
2019-05-17 12:54:08 +0100

    create Footer Component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return (
-      <nav style={{ backgroundColor: '#36474F' }}>
-        <div class="container">
-          <div class="nav-wrapper">
-            <p class="brand-logo">© Dare Lawal 2019. All rights reserved.</p>
-          </div>
+      <footer className="page-footer" style={{ backgroundColor: '#36474F' }}>
+        <div class="footer-copyright">
+          <div class="container">© Dare Lawal. All rights reserved.</div>
         </div>
-      </nav>
+      </footer>
     );
   }
 }
