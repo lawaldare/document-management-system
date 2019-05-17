d8a6eaf098a87bb340876dbc4659c158b8bd846f
Dare Lawal <lawaldare@gmail.com>
2019-05-17 12:46:16 +0100

    create NavBar Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './NavBar/NavBar';
+import Footer from './Footer/Footer';
 
 function App({ children }) {
   return (
     <div className="App">
       <NavBar />
       {children}
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <nav style={{ backgroundColor: '#36474F' }}>
+        <div class="container">
+          <div class="nav-wrapper">
+            <Link to="/" class="brand-logo">
+              dms
+            </Link>
+            <ul class="right hide-on-med-and-down">
+              <li>
+                <Link to="/signup">Register</Link>
+              </li>
+              <li>
+                <Link to="/signin">Login</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/signout">Logout</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
